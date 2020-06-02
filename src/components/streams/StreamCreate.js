f4413e73364d4bd08c506a81fa992279800c2532
jamiekim828 <jamiekim828@gmail.com>
2020-06-02 13:22:14 +0200

    StreamCreate.js

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,23 +2,47 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-  renderInput(formProps) {
-    console.log(formProps);
+  // renderInput(formProps) {
+  //   return (
+  //     <input
+  //       {...formProps.input}
+  //       // onChange={formProps.input.onChange}
+  //       // value={formProps.input.value}
+  //     />
+  //   );
+  // }
+
+  // can be destructured like this
+  renderInput({ input, label }) {
     return (
-      <input
-        // onChange={formProps.input.onChange}
-        // value={formProps.input.value}
-        {...formProps.input}
-      />
+      <div className='field'>
+        <label>{label}</label>
+        <input {...input} />
+      </div>
     );
+  }
+
+  onSubmit(formValue) {
+    // event.preventDefault(); -> redux form will automatically do this
+    console.log('formValue', formValue);
   }
 
   render() {
     console.log(this.props);
     return (
-      <form>
-        <Field name='title' component={this.renderInput} />
-        <Field name='description' component={this.renderInput} />
+      <form
+        // this.props.handleSubmit comes from Redux Form
+        // check this from the console
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className='ui form '
+      >
+        <Field name='title' component={this.renderInput} label='Enter Title' />
+        <Field
+          name='description'
+          component={this.renderInput}
+          label='Ender Description'
+        />
+        <button className='ui button primary'>Submit</button>
       </form>
     );
   }
