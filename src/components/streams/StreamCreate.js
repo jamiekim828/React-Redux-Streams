ba095fdb9f1aed6bdfa36270ccdd3df63b7e4aae
jamiekim828 <jamiekim828@gmail.com>
2020-06-05 14:33:24 +0200

    StreamForm works in StreamEdit component

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { createStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamCreate extends React.Component {
+  // move StreamCreate to StreamForm to reuse in StreamEdit
   // renderInput(formProps) {
   //   return (
   //     <input
@@ -15,28 +16,28 @@ class StreamCreate extends React.Component {
   // }
 
   // use meta.touched for error handling
-  renderError({ error, touched }) {
-    if (touched && error) {
-      return (
-        <div className='ui error message'>
-          <div className='header'>{error}</div>
-        </div>
-      );
-    }
-  }
+  // renderError({ error, touched }) {
+  //   if (touched && error) {
+  //     return (
+  //       <div className='ui error message'>
+  //         <div className='header'>{error}</div>
+  //       </div>
+  //     );
+  //   }
+  // }
 
   // can be destructured like this
-  renderInput = ({ input, label, meta }) => {
-    console.log('meta', meta);
-    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
-    return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} autoComplete='off' />
-        {this.renderError(meta)}
-      </div>
-    );
-  };
+  // renderInput = ({ input, label, meta }) => {
+  //   console.log('meta', meta);
+  //   const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+  //   return (
+  //     <div className={className}>
+  //       <label>{label}</label>
+  //       <input {...input} autoComplete='off' />
+  //       {this.renderError(meta)}
+  //     </div>
+  //   );
+  // };
 
   onSubmit = (formValues) => {
     // event.preventDefault(); -> redux form will automatically do this
@@ -48,46 +49,28 @@ class StreamCreate extends React.Component {
   render() {
     console.log(this.props);
     return (
-      <form
-        // this.props.handleSubmit comes from Redux Form
-        // check this from the console
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className='ui form error'
-      >
-        <Field name='title' component={this.renderInput} label='Enter Title' />
-        <Field
-          name='description'
-          component={this.renderInput}
-          label='Ender Description'
-        />
-        <button className='ui button primary'>Submit</button>
-      </form>
+      <div>
+        <h3>Create a Stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
     );
   }
 }
 
 // meta.error
 // we also can use meta.touched
-const validate = (formValues) => {
-  const errors = {};
+// const validate = (formValues) => {
+//   const errors = {};
 
-  if (!formValues.title) {
-    // only ran if the user did not enter the title
-    errors.title = 'You must enter a title';
-  }
-  if (!formValues.description) {
-    errors.description = 'You must enter a description';
-  }
+//   if (!formValues.title) {
+//     // only ran if the user did not enter the title
+//     errors.title = 'You must enter a title';
+//   }
+//   if (!formValues.description) {
+//     errors.description = 'You must enter a description';
+//   }
 
-  return errors;
-};
+//   return errors;
+// };
 
-const mapStateToProps = () => {
-  return {};
-};
-
-const formWrapped = reduxForm({ form: 'streamCreate', validate: validate })(
-  StreamCreate
-);
-
-export default connect(mapStateToProps, { createStream })(formWrapped);
+export default connect(null, { createStream })(StreamCreate);
